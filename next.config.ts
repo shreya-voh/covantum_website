9c7d36312a66bd533a6bc6971e6046aecc761af3
shreya-mad <smadeshiya12345@gmail.com>
2026-06-10 17:34:21 +0530

    config added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   output: "export",
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
